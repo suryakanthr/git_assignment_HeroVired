7fd53460baf646308cc77a5213f1e48d79a39a6d
surya <suryakanthr21@gmail.com>
2025-02-26 23:19:58 +0530

    Divide function fixed

--- calculatorPlus.py
+++ calculatorPlus.py
@@ -15,7 +15,8 @@ def multiply(self, a, b):
     return a * b
 
 def divide(self, a, b):
-
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
     return a / b
 
 # TODO: Implement the following function to calculate the square root of a number.
